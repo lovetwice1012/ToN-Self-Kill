3902a5b2213884b5bde6b864614b2f43ca7e2c75
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2025-04-12 00:14:50 +0000

    thonking

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use enigo::{Enigo, Keyboard, Key};
+use enigo::{
+    Direction::{Press, Release},
+    Enigo, Key, Keyboard, Settings,
+};
 use std::thread;
 use std::time::Duration;
-
 fn main() {
-    let mut enigo = Enigo::new();
-    enigo.key_down(Key::Other(187));
-    enigo.key_down(Key::Other(222));
+    let mut enigo = Enigo::new(&Settings::default()).unwrap();
+    enigo.key(Key::Other(187), Press).unwrap();
+    enigo.key(Key::Other(222), Press).unwrap();
     thread::sleep(Duration::from_secs(5));
-    enigo.key_up(Key::Other(187));
-    enigo.key_up(Key::Other(222));
+    enigo.key(Key::Other(187), Release).unwrap();
+    enigo.key(Key::Other(222), Release).unwrap();
 }
