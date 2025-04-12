83d895a11e84c2f3e347d8521e369cfa7765a15e
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2025-04-12 00:10:14 +0000

    てｓｔ

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,10 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    enigo::key(Key::Other(187), Press).unwrap();
-    enigo::key(Key::Other(222), Press).unwrap();
+    let mut enigo = Enigo::new();
+    enigo.key(Key::Other(187), Press).unwrap();
+    enigo.key(Key::Other(222), Press).unwrap();
     thread::sleep(Duration::from_secs(5));
-    enigo::key(Key::Other(187), Release).unwrap();
-    enigo::key(Key::Other(222), Release).unwrap();
+    enigo.key(Key::Other(187), Release).unwrap();
+    enigo.key(Key::Other(222), Release).unwrap();
 }
