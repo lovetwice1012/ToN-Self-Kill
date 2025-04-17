7694830c3fbca1522309f51c7d304ae8c2011949
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2025-04-17 15:48:49 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
-use enigo::{
-    Direction::{Press, Release},
-    Enigo, Key, Keyboard, Settings,
-};
+use enigo::{Direction::{Press, Release}, Enigo, Key, Settings};
 use std::thread;
 use std::time::Duration;
-fn main() {
+
+#[no_mangle]
+pub extern "C" fn press_keys() {
     let mut enigo = Enigo::new(&Settings::default()).unwrap();
-    enigo.key(Key::Other(187), Press).unwrap();
-    enigo.key(Key::Other(222), Press).unwrap();
+    enigo.key(Key::Other(187), Press).unwrap();  // F1
+    enigo.key(Key::Other(222), Press).unwrap();  // F2
     thread::sleep(Duration::from_secs(5));
     enigo.key(Key::Other(187), Release).unwrap();
     enigo.key(Key::Other(222), Release).unwrap();
